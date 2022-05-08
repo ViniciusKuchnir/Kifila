47f8095b2e26dc659310689bd4a64b4aa13532d0
Vinícius Kuchnir <vina200902@gmail.com>
2022-05-08 19:48:39 -0300

    Mudança do navbar para empresas

--- backend/getDataUser.php
+++ backend/getDataUser.php
@@ -14,7 +14,7 @@
         $fetch_user = mysqli_fetch_row($query_user);
         $fetch_company = mysqli_fetch_row($query_company);
 
-        
+
         if ($total_users == 1) {
             $nameUser = $fetch_user[1] . ' ' .$fetch_user[2];
             return $nameUser;
@@ -24,5 +24,24 @@
         }
 
         mysqli_close($conn); 
+    }
+
+    function verificaTipoUsuario(){
+        require ('conectionBD.php');
+        $email = $_COOKIE['email'];       
+        
+        $query_user = mysqli_query($conn, "SELECT * FROM usuario WHERE email = '$email' LIMIT 1");
+        $total_users = mysqli_num_rows($query_user);  
+
+        $query_company = mysqli_query($conn, "SELECT * FROM empresa WHERE email = '$email' LIMIT 1");
+        $total_company = mysqli_num_rows($query_company);
+
+        if ($total_users == 1) {
+            return 1;
+        }else if($total_company == 1){
+            return 2;
+        }
+        
+        mysqli_close($conn);
     }
 ?>
--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -2,6 +2,10 @@
     <?php include 'style.css'; ?>
 </style>
 
+<?php 
+    include_once ('C:/xampp/htdocs/Kifila/backend/getDataUser.php')
+?>
+
 <nav>
     <div class="navbar">
         <div class="logo">
@@ -9,6 +13,18 @@
         </div>
         <div class="navbar-menu">
             <ul class="menu">
+                <?php if(verificaTipoUsuario() == 2) { ?>
+                    <li class="menu-item">
+                        <a href="../../pages/dashboard_pj">
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="../../pages/filas">
+                            Filas
+                        </a>
+                    </li>
+                <?php }?>
                 <li class="menu-item">
                     <a href="../../pages/user">
                         Perfil
