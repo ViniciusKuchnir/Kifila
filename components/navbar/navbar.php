fb5c5fc471e3add2889badbbb938df0580db43d4
Vinícius Kuchnir <vina200902@gmail.com>
2022-05-08 21:32:25 -0300

    Alteração responsividade navbar

--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -6,31 +6,60 @@
     include_once ('C:/xampp/htdocs/Kifila/backend/getDataUser.php')
 ?>
 
-<nav>
-    <div class="navbar">
-        <div class="logo">
-            <h2 id="logo">Kifila</h2>
-        </div>
-        <div class="navbar-menu">
-            <ul class="menu">
-                <?php if(verificaTipoUsuario() == 2) { ?>
-                    <li class="menu-item">
-                        <a href="../../pages/dashboard_pj">
-                            Dashboard
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="../../pages/filas">
-                            Filas
-                        </a>
-                    </li>
-                <?php }?>
-                <li class="menu-item">
-                    <a href="../../pages/user">
-                        Perfil
-                    </a>
-                </li>       
-            </ul>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 head">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="logo">
+                        <h2 id="logo">Kifila</h2>
+                    </div>
+                </div>
+                <div class="col-md-10">
+                    <ul class="menu-items">
+                        <?php if(verificaTipoUsuario() == 2) { ?>
+                            <li class="menu-item">
+                                <a href="../../pages/dashboard_pj">
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="../../pages/filas">
+                                    Filas
+                                </a>
+                            </li>
+                        <?php }?>
+                        <li class="menu-item">
+                            <a href="../../pages/user">
+                                Perfil
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>        
         </div>
     </div>
-</nav>
+</div>
+
+
+
+
+<!-- <ul class="menu">
+<?php if(verificaTipoUsuario() == 2) { ?>
+                                    <li class="menu-item">
+                                        <a href="../../pages/dashboard_pj">
+                                            Dashboard
+                                        </a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="../../pages/filas">
+                                            Filas
+                                        </a>
+                                    </li>
+                                <?php }?>
+                                <li class="menu-item">
+                                    <a href="../../pages/user">
+                                        Perfil
+                                    </a>
+                                </li>       
+                            </ul> -->
