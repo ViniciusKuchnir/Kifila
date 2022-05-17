6f12de13674e97ae1713434263741a8bca607efb
Vinícius Kuchnir <vina200902@gmail.com>
2022-05-17 17:38:57 -0300

    Criação da estrutura da página de pesquisar fila

---
+++ pages/pesquisarFilas/index.php
@@ -0,0 +1,19 @@
+<?php  
+    require('C:/xampp/htdocs/Kifila/backend/conectionBD.php');
+    require('C:/xampp/htdocs/Kifila/backend/filas.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php require('../../components/head.php'); ?>
+    <title>Kifila - Pesquisar Filas</title>
+</head>
+<body>
+    <?php require('C:/xampp/htdocs/Kifila/components/navbar/navbar.php') ?>
+        
+    
+    
+    <?php require('../../components/scripts.php');?>
+</body>
+</html>
