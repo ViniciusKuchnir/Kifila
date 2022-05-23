10b8e8761ed7177de8debf91704511032d951fb5
Vinícius Kuchnir <vina200902@gmail.com>
2022-05-23 16:28:27 -0300

    Redirecionamento para a página da lista de filas quando uma nova lista é criada

--- backend/excluirFila.php
+++ backend/excluirFila.php
@@ -5,6 +5,7 @@
 
     mysqli_query($conn, "DELETE FROM fila WHERE idfila='$idfila'");
 
+    echo "<script>window.location.href='../pages/filas/index.php';</script>";
 
     mysqli_close($conn);
 ?>
