9b0fa1136541e1e109c96a1ddff81cda5255ed80
Vinícius Kuchnir <vina200902@gmail.com>
2022-05-24 22:56:09 -0300

    Alteração navbar

--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -25,7 +25,7 @@
                             </li>
                             <li class="menu-item">
                                 <a href="../../pages/filas">
-                                    Filas
+                                    Criar
                                 </a>
                             </li>
                         <?php }?>
