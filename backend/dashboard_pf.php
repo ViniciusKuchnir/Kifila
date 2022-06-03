4bd0545cadacc5748b40fe3a1605b5e967a7f2d0
Vinícius Kuchnir <vina200902@gmail.com>
2022-06-03 12:16:39 -0300

    Início da listagem de filas em que o usuário está presente

--- backend/dashboard_pf.php
+++ backend/dashboard_pf.php
@@ -1,3 +1,32 @@
 <?php
-    echo "Olá Dashboard!!!!";
+    function nameUser($email){
+        require('conectionBD.php');
+        $query = mysqli_fetch_assoc(mysqli_query($conn,
+        "SELECT nome, sobrenome 
+        FROM usuario
+        WHERE usuario.email = '$email'"
+        ));
+
+        print_r($query['nome']." ".$query['sobrenome']);
+        mysqli_close($conn);
+    }
+
+    //Função que retorna as listas em que o usuário está inserido 
+    //Para mostrar no dashboard em forma de card
+    function listQueuesUser($email){
+        require('conectionBD.php');
+        $query = mysqli_fetch_assoc(mysqli_query($conn,
+        "SELECT  *
+        FROM ingresso i
+        JOIN usuario u
+        JOIN fila f
+        ON u.codUser = i.iduser
+        WHERE u.email = $email AND i.atendido = 0 
+        
+        "
+        ));
+
+        print_r($query['nome']." ".$query['sobrenome']);
+        mysqli_close($conn);
+    }
 ?>
--- pages/dashboard_pf/index.php
+++ pages/dashboard_pf/index.php
@@ -1,3 +1,8 @@
+<?php
+    require('../../backend/conectionBD.php');
+    require('../../backend/dashboard_pf.php');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,7 +12,40 @@
 <body>
     <?php require('../../components/navbar/navbar.php') ?>
 
-    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 mt-3">
+                <h2>Olá, <?php echo nameUser($_COOKIE['email'])?></h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 mt-3">
+                <h2>Filas que você se encontra:</h2>
+                <div class="queue-user">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Fila SUS - Uberaba</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Você está na posição:</h6>
+                                    <div class="position">
+                                        <i class="fa-solid fa-person-arrow-down-to-line"></i>
+                                        <p class="position-number">8</p>
+                                    </div>
+                                    <div class="buttons">
+                                        <a href="#">
+                                            <button class="btn">Sair da Fila</button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php require('../../components/scripts.php');?>
 </body>
 </html>
